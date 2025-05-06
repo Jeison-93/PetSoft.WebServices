f1a38271ce0a8ae36c3823336f7df4ffabbabda6
Jeison-93 <jeison_manjarres@soy.sena.edu.co>
2025-05-05 22:26:19 -0500

    ajuste user

--- Application/UserAppService.cs
+++ Application/UserAppService.cs
@@ -1,95 +1,129 @@
-﻿using PetSoft.WebServices.Application.Interface;
+﻿using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Asn1.Ocsp;
+using PetSoft.WebServices.Application.Interface;
 using PetSoft.WebServices.Data.Dto;
 using PetSoft.WebServices.Data.Models;
+using PetSoft.WebServices.Helpers;
 using System.Xml.Linq;
 
 namespace PetSoft.WebServices.Application
 {
     public class UserAppService : IUserAppService
     {
-        readonly PetsoftdbContext _context;
+        private readonly PetsoftdbContext _context;
 
         public UserAppService(PetsoftdbContext context)
         {
             _context = context;
         }
-        public UserGetDto Get(int id)
+        public RequestResponse<UserGetDto> Get(int id)
         {
-            var result = _context.User
-                .Where(f => f.Id == id)
-                    .Select(s => new UserGetDto()
-                  {
-                        Id = s.Id,
-                        DocumentType = s.DocumentType,
-                        DocumentNumber = s.DocumentNumber,
-                        Name = s.Name,
-                        LastName = s.LastName,
-                        Password = s.Password,
-                        Phone = s.Phone,
-                        Email = s.Email,
-                        Addresss = s.Address,
-                        UserType = s.UserType,
-                        State = s.State,
-                        StateDescription = s.State == 1 ? "Activo" : "Inactivo"
-                  }).FirstOrDefault();// instruccion para recuperar el primer dato de la consulta
+            RequestResponse<UserGetDto> response = new();
+            try
+            {
+                var result = _context.User.AsNoTracking()
+                    .Where(f => f.Id == id)
+                        .Select(s => new UserGetDto()
+                        {
+                            Id = s.Id,
+                            DocumentType = s.DocumentType,
+                            DocumentNumber = s.DocumentNumber,
+                            Name = s.Name,
+                            LastName = s.LastName,
+                            Password = s.Password,
+                            Phone = s.Phone,
+                            Email = s.Email,
+                            Addresss = s.Address,
+                            UserType = s.UserType,
+                            State = s.State,
+                            StateDescription = s.State == 1 ? "Activo" : "Inactivo"
+                        }).FirstOrDefault();// instruccion para recuperar el primer dato de la consulta
 
-            return result != null ? result : new UserGetDto();
+                if (result == null)
+                    return response.CreateUnsuccessful("No se encontró información en la base de datos");
+
+                return response.CreateSuccessful(result);
+            }
+            catch (Exception ex)
+            {
+                return response.CreateError(ex.Message);
+            }
         }
 
-        public IEnumerable<UserGetDto> GetAll()
+        public RequestResponse<IEnumerable<UserGetDto>> GetAll()
         {
-            var result = _context.User
+            RequestResponse<IEnumerable<UserGetDto>> response = new();
+            try
+            {
+                var result = _context.User.AsNoTracking()
                  .Select(s => new UserGetDto()
                  {
                      Id = s.Id,
                      DocumentType = s.DocumentType,
                      DocumentNumber = s.DocumentNumber,
-                     Name = s.Name, 
+                     Name = s.Name,
                      LastName = s.LastName,
                      Password = s.Password,
                      Phone = s.Phone,
                      Email = s.Email,
-                     Addresss = s.Address, 
+                     Addresss = s.Address,
                      UserType = s.UserType,
                      State = s.State,
                      StateDescription = s.State == 1 ? "Activo" : "Inactivo"
 
                  });
 
-            return result;
+                if (!result.Any())
+                    return response.CreateUnsuccessful("No se encontró información en la base de datos");
+
+                return response.CreateSuccessful(result);
+            }
+            catch (Exception ex)
+            {
+
+                return response.CreateError(ex.Message);
+            }
+
+
 
         }
 
-        public string Save(UserSaveDto parameter)
+        public RequestResponse<string> Save(UserSaveDto parameter)
         {
-            if (ExistUser(parameter.Email))
+            RequestResponse<string> response = new();
+            try
             {
-                return "El correo ya está registrado";
-            }
+                if (ExistUser(parameter.DocumentNumber))
+                    return response.CreateUnsuccessful($"Ya existe un usuario con el número de documento {parameter.DocumentNumber}");
 
-            if (ExistUserByDoc(parameter.DocumentNumber))
+
+
+                if (ExistUserByEmail(parameter.Email))
+                    return response.CreateUnsuccessful($"El correo {parameter.Email} ya se encutrana registrado");
+
+                var oUser = new User();
+
+
+                oUser.DocumentType = parameter.DocumentType;
+                oUser.DocumentNumber = parameter.DocumentNumber;
+                oUser.Name = parameter.Name;
+                oUser.LastName = parameter.LastName;
+                oUser.Password = parameter.Password;
+                oUser.Phone = parameter.Phone;
+                oUser.Email = parameter.Email;
+                oUser.Address = parameter.Addresss;
+                oUser.UserType = parameter.UserType;
+                oUser.State = 1;
+
+                _context.User.Add(oUser);
+                _context.SaveChanges();
+
+                return response.CreateSuccessful("El usuario se creó exitosamente");
+            }
+            catch (Exception ex)
             {
-                return "el numero de documento ya existe en otro registro";
+                return response.CreateError(ex.Message);
             }
-
-
-            User user =new();
-            user.DocumentType = parameter.DocumentType;
-            user.DocumentNumber = parameter.DocumentNumber;
-            user.Name = parameter.Name;
-            user.LastName = parameter.LastName;
-            user.Password = parameter.Password;
-            user.Phone = parameter.Phone;
-            user.Email = parameter.Email;
-            user.Address = parameter.Addresss;
-            user.UserType = parameter.UserType;
-            user.State = 1;
-
-            _context.User.Add(user);
-            _context.SaveChanges();
-
-
-            return "Registro Exitoso";
         }
         
         private bool ExistUserByDoc(string documentNumber)
@@ -97,63 +131,84 @@ namespace PetSoft.WebServices.Application
             return _context.User.Any(f => f.DocumentNumber == documentNumber);
         }
 
-        private bool ExistUser(string email)
-        {
-            return _context.User.Any(f => f.Email == email);
-        }
 
-        public string Update(UserUpdateDto parameter)
+
+        public RequestResponse<string> Update(UserUpdateDto parameter)
         {
+            RequestResponse<string> response = new();
             try
             {
-                User user = _context.User.FirstOrDefault(f => f.Id == parameter.Id);
-                if (user == null)
+                var oUser = _context.User.AsNoTracking().FirstOrDefault(f => f.Id == parameter.Id);
+
+                if (oUser == null)
                 {
-                    return "El usuario no existe";
+                    return response.CreateUnsuccessful("No se encontró información para el usuario");
                 }
 
-                user.Password = parameter.Password;
-                user.Phone = parameter.Phone;
-                user.Address = parameter.Address;
-                user.UserType = parameter.UserType;
-                user.State = parameter.State;
+                oUser.Password = parameter.Password;
+                oUser.Phone = parameter.Phone;
+                oUser.Address = parameter.Address;
+                oUser.UserType = parameter.UserType;
+                oUser.State = parameter.State;
+                oUser.Email = parameter.Email;
+                oUser.DocumentType = parameter.DocumentType;
+               
 
-                _context.User.Update(user);
+                _context.User.Update(oUser);
+                _context.Entry(oUser).Property(r => r.Id).IsModified = false;
                 _context.SaveChanges();
 
-                return "Se actualizó correctamente";
+                return response.CreateSuccessful("La información del usuario se actualizó exitosamente");
             }
             catch (Exception ex)
             {
 
-                return ex.Message;
+                return response.CreateError(ex.Message);
             }
             
         }
 
-        public string ChangeState(int Id)
+        public RequestResponse<string> ChangeState(int Id)
         {
+            RequestResponse<string> response = new();
             try
             {
-                User user = _context.User.FirstOrDefault(f => f.Id == Id);
-                if (user == null)
-                {
-                    return "El usuario no existe";
-                }
+                var oUser = _context.User.AsNoTracking().FirstOrDefault(f => f.Id == Id);
 
-                user.State = (sbyte)(user.State == 1 ? 0 : 1);
+                if (oUser == null)
+                
+                    return response.CreateUnsuccessful("No se encontró información para el usuario");
 
 
-                _context.User.Update(user);
+                oUser.State = (sbyte)(oUser.State == 1 ? 0 : 1);
+
+
+                _context.User.Update(oUser);
+                _context.Entry(oUser).Property(r => r.Id).IsModified = false;
                 _context.SaveChanges();
 
-                return "Se actualizó correctamente";
+                return response.CreateSuccessful("El cambio de estado se realió exitosamente");
             }
             catch (Exception ex)
             {
 
-                return ex.Message;
+                return response.CreateError(ex.Message);
             }
         }
+
+        
+
+            private bool ExistUser(string documentNumber)
+            => _context.User.AsNoTracking().Any(f => f.DocumentNumber == documentNumber);
+
+            private bool ExistUserByEmail(string email)
+            => _context.User.AsNoTracking().Any(f => f.Email == email);
+
+
+           
+        
+
+        
+        
     }
 }
--- Data/Dto/User/UserUpdateDto.cs
+++ Data/Dto/User/UserUpdateDto.cs
@@ -7,11 +7,13 @@
         public string? Address { get; set; }
 
         public string Phone { get; set; } = null!;
+        public string Email { get; set; } = null!;
 
         public string? Password { get; set; }
 
         public string UserType { get; set; } = null!;
 
         public sbyte State { get; set; }
+        public string DocumentType { get; set; } = null!;
     }
 }
