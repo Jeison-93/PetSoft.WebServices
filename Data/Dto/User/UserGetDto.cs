512be0a9b92534ac0fc89d75837b62b217688bcb
EMILIO18181 <emilio_jlopez@soy.sena.edu.co>
2025-05-25 12:28:42 -0500

    Update UserGetDto.cs

--- Data/Dto/User/UserGetDto.cs
+++ Data/Dto/User/UserGetDto.cs
@@ -14,7 +14,7 @@
 
         public string Email { get; set; } = null!;
 
-        public string? Addresss { get; set; }
+        public string? Address { get; set; }
 
         public string Phone { get; set; } = null!;
 
